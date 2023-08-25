2046d628853f636a28d67ec9f8ed55b2e9120a96
Shivesh kr jha <shiveshkrjha2015@gmail.com>
2023-08-25 11:51:22 +0530

    add functionality including onChange, onSubmit function to the createListingPage

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
